835ac61d6ee45b5e12a2442ddc81f8aa054173a6
Alper Ahmetoglu <ahmetoglu.alper@gmail.com>
2016-07-14 14:39:28 +0300

    add math helper

--- app/src/main/java/com/example/melih/beacon_tool/GraphActivity.java
+++ app/src/main/java/com/example/melih/beacon_tool/GraphActivity.java
@@ -3,15 +3,21 @@ package com.example.melih.beacon_tool;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by alper on 14/07/16.
  */
-public class GraphActivity extends LeScanner {
+public class GraphActivity extends AppCompatActivity {
+
+    private TextView textView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-        
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText(MathHelper.lol());
     }
 }
---
+++ app/src/main/java/com/example/melih/beacon_tool/MathHelper.java
@@ -0,0 +1,10 @@
+package com.example.melih.beacon_tool;
+
+/**
+ * Created by alper on 14/07/16.
+ */
+public class MathHelper {
+    public static String lol(){
+        return "Hello World!";
+    }
+}
