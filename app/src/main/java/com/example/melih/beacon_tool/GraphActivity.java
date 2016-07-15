835ff9ba5e15cb741629a3ce348c6cc67fb0b999
melihmutlu <m.melihmutlu@gmail.com>
2016-07-15 10:52:18 +0300

    spinner added

--- app/src/main/java/com/example/melih/beacon_tool/GraphActivity.java
+++ app/src/main/java/com/example/melih/beacon_tool/GraphActivity.java
@@ -2,22 +2,27 @@ package com.example.melih.beacon_tool;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by alper on 14/07/16.
  */
-public class GraphActivity extends AppCompatActivity {
+public class GraphActivity extends LeScanner {
 
-    private TextView textView;
+    private Spinner spinner;
+    private ArrayAdapter<Beacon> adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = new ArrayAdapter<Beacon>( this, R.layout.support_simple_spinner_dropdown_item, beaconList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        textView = (TextView) findViewById(R.id.textView);
-        textView.setText(MathHelper.lol());
+
+
     }
 }
