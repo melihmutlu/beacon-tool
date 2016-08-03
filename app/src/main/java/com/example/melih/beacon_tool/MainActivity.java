a3619c517ffb69daf9265fff9e23001b8ed1e137
melihmutlu <m.melihmutlu@gmail.com>
2016-08-03 11:36:27 +0300

    deleted unused files

--- app/src/main/java/com/example/melih/beacon_tool/MainActivity.java
+++ app/src/main/java/com/example/melih/beacon_tool/MainActivity.java
@@ -15,22 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
         btn3 = (Button) findViewById(R.id.btn3);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this , GraphActivity.class);
-                // put extras
-                startActivity(i);
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this , BeaconMap.class);
                 // put extras
                 startActivity(i);
             }
